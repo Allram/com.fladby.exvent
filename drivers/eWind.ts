3832e6903e5e926de3995a18f935d286eab4bdde
Vegard Fladby <vegard.fladby@gmail.com>
2022-12-25 22:42:40 +0100

    v2.7.4 -     Fixed a bug in target_temperature which forced the modbus-connection to force-close if values were out of range.

--- drivers/eWind.ts
+++ drivers/eWind.ts
@@ -64,7 +64,7 @@ export class eWind extends Homey.Device {
 
         if (result['temperature_setpoint'] && result['temperature_setpoint'].value !== 'xxx') {
             let temperature = ((Number(result['temperature_setpoint'].value) / 10));
-            if (temperature >= 15 && temperature <= 25) {
+            if (temperature >= 15 && temperature <= 22) {
                 this.setCapabilityValue('target_temperature',temperature);
             }
         }
