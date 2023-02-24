f14cd6bd8276af184cd0deb526b87c4e2db89549
Vegard Fladby <vegard.fladby@gmail.com>
2023-02-24 09:32:59 +0100

    Added Debugging from VScode

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
 import Homey from 'homey';
 
+
 class MyExventApp extends Homey.App {
 
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
+    // Start debuger
+if (process.env.DEBUG === '1'){
+  try{ 
+    require('inspector').waitForDebugger();
+  }
+  catch(error){
+    require('inspector').open(9225, '0.0.0.0', true);
+  }
+}
     this.log('MyExventApp has been initialized');
   }
 
